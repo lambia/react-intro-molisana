cf7c7358e4dfd146911adc69050a7dc92719dae3
Luca Lambia <luca@lambia.it>
2025-12-16 18:17:18 +0100

    minor (layout)

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -94,10 +94,7 @@ function Main() {
 		<main className={style.main}>
 			<div className="boxed">
 
-				<h1>Benvenuto in La Molisana</h1>
-				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, veritatis iusto! Soluta suscipit iure error delectus quis, earum quod ea dolor maiores voluptatibus atque debitis cupiditate neque beatae iusto repellat.</p>
-
-				<h2>Prodotti</h2>
+				<h2>Lista Prodotti</h2>
 
 				<div className={style.productsWrapper}>
 
